9aef93aabbb4cded1f253a17b05f291a38973334
Duy199 <154971599+Duy199@users.noreply.github.com>
2024-04-26 07:20:17 +0700

    Ongoing exercise 1.11

--- src/NhapNgayThangNam.java
+++ src/NhapNgayThangNam.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 public class NhapNgayThangNam {
+    int Year;
+    int Month;
+    int Day;
     public void NgayThangNam (){
         Scanner input = new Scanner(System.in);
-        int Year = input.nextInt();
-        int Month = input.nextInt();
-        int Day = input.nextInt();
+        Year = input.nextInt();
+        Month = input.nextInt();
+        Day = input.nextInt();
         XacDinhNgay Ngay = new XacDinhNgay();
         int maxNgayTrongThang = Ngay.ngayTrongThang(Month, Year);
 
         if (Day > 0 && Day <= maxNgayTrongThang && Month > 0 && Month <= 12 && Year >0) {
-           System.out.println("dd/mm/yy: " + Day + "/" + Month + "/" + Year);
            System.out.println(Day + " la mot ngay hop le");
+           System.out.println("dd/mm/yy: " + Day + "/" + Month + "/" + Year);
         }
         else {
             System.out.println(Day + " la mot ngay ko hop le");
         }
 
+    }
+    public int HaveDay() {
+        return Day;
+    }
+    public int HaveMonth() {
+        return  Month;
+    }
+    public int HaveYear() {
+        return Year;
     }
 }

f56fb6353235b7a6fdd9e3e43711bd1e4b6bf6b5
Duy199 <154971599+Duy199@users.noreply.github.com>
2024-04-29 22:34:09 +0700

    Completed exercise 1.12 - Final version with more clean code

--- src/DemUocSoNguyen.java
+++ src/DemUocSoNguyen.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
 public class DemUocSoNguyen {
-    public void XacDinhUocSoNguyen() {
+    static int NhapSoNguyen () {
+        System.out.println("Moi Nhap so Nguyen");
         Scanner input = new Scanner(System.in);
-        System.out.println("Nhap so Nguyen");
         int SoNguyen = input.nextInt();
+        return SoNguyen;
+    }
+    public void XacDinhUocSoNguyen() {
+        int SoNguyen = NhapSoNguyen();
         while (SoNguyen < 1) {
-            SoNguyen = input.nextInt();
+            SoNguyen = NhapSoNguyen();
         }
         int DemUoc = 0;
-        System.out.println("Uoc so cua " + SoNguyen);
+        System.out.println("Cac so chia het cho " + SoNguyen);
         for (int i = 1; i <= SoNguyen; i++) {
             if (SoNguyen % i == 0) {
                 DemUoc ++;
                 System.out.println(i);
             }
-
         }
+        System.out.println("=> So luong cua cac uoc so cua so " + SoNguyen + " la " + DemUoc);
     }
 }
 

934b65f241ccd15bc9024a87c59e04cdfd29c6d7
Duy199 <154971599+Duy199@users.noreply.github.com>
2024-05-02 20:06:05 +0700

    90% Completed for Calculator program - will add more feature for this

--- src/MayTinh.java
+++ src/MayTinh.java
@@ -5,46 +5,94 @@ public class MayTinh {
         Scanner input = new Scanner(System.in);
         System.out.println("Nhap so ");
         int Number = input.nextInt();
-        System.out.println("Moi nhap phep tinh (1. +, 2. -, 3. *, 4. /) hoac dau (=) de cho ra ket qua");
-        int Calculate = input.nextInt();
+        System.out.println("Moi nhap phep tinh (+, -, *, /) hoac dau (=) de cho ra ket qua");
+        input.nextLine();
+        String Calculate = input.nextLine();
         int Result = 0;
-        // Neu ko nhap dau =
-        while (Calculate < 5) {
+        int DemSoLanLap = 0;
 
+        // Neu ko nhap dau =
+        while (!Calculate.equals("=")) {
+            DemSoLanLap ++;
             // phep tinh cong
-            if (Calculate == 1) {
-                Result = Result + Number;
-                System.out.println(Result);
+            if (Calculate.equals("+")) {
+                if (Result == 0) {
+                    Result = Result + Number;
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    Result = Result + Number;
+
+                }
+                else {
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    Result = Result + Number;
+                }
             }
             // phep tinh tru
-            else if (Calculate == 2) {
-                Result = Result - Number;
-                //System.out.println(Result);
+            else if (Calculate.equals("-")) {
+                if (Result == 0) {
+                    Result = Result + Number;
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    Result = Result - Number;
+                }
+                else {
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    Result = Result - Number;
+                }
             }
             // phep tinh nhan
-            else if (Calculate == 3) {
-                Result = Result * Number;
-                //System.out.println(Result);
+            else if (Calculate.equals("*")) {
+                if (Result == 0) {
+                    Result = Result + Number;
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    Result = Result * Number;
+                }
+                else {
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    Result = Result * Number;
+                }
             }
             // phep tinh chia
             else {
-                Result = Result / Number;
-                //System.out.println(Result);
+                if (Result == 0) {
+                    Result = Result + Number;
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    // Chặn người dùng nhập số 0 ngay lập tức
+                    while (Number == 0) {
+                        System.out.println(Result +" Không thể chia cho 0. Vui lòng nhập một số khác: ");
+                        Number = input.nextInt();
+                    }
+                    Result = Result / Number;
+                }
+                else {
+                    System.out.println("Moi nhap so tiep theo ");
+                    Number = input.nextInt();
+                    // Chặn người dùng nhập số 0 ngay lập tực
+                    while (Number == 0) {
+                        System.out.println(Result + " Không thể chia cho 0. Vui lòng nhập một số khác: ");
+                        Number = input.nextInt();
+                    }
+                    Result = Result / Number;
+                }
             }
-
-            System.out.println("Moi nhap so tiep theo ");
-            Number = input.nextInt();
-
-            System.out.println("Moi nhap phep tinh ");
-            Calculate = input.nextInt();
-
+            System.out.println("Ket qua hien tai la " + Result);
+            System.out.println("Moi nhap phep tinh (+, -, *, /) hoac dau (=) de cho ra ket qua");
+            input.nextLine();
+            Calculate = input.nextLine();
+            //System.out.println("Vong lap " + DemSoLanLap + " co ket qua la " + Result);
         }
         // Neu nhap dau =
-        if (Result == 0) {
-            System.out.println("Ket qua la " + Number);
+        if (Result == 0 & DemSoLanLap == 0) {
+            System.out.println("Ket qua Final la " + Number);
         }
         else {
-            System.out.println("Ket qua la " + Result);
+            System.out.println("Ket qua Final la " + Result);
         }
     }
 }
